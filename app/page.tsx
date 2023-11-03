a33edc2c296f310e58f9b476735c19d19f997d71
William Morales <william.rolando88@hotmail.com>
2023-11-03 18:17:15 -0500

    Search and pagination

--- app/page.tsx
+++ app/page.tsx
@@ -36,6 +36,7 @@ export default function Page() {
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop and mobile versions"
+            priority={true}
           />
           <Image
             src="/hero-mobile.png"
