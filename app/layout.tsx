18172cba10aaf3665d4e4275f9621f48e38eba59
William Morales <william.rolando88@hotmail.com>
2023-11-04 19:29:24 -0500

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,17 @@
 import "@/app/ui/global.css";
+import { Metadata } from "next";
 import { FC } from "react";
 import { LayoutInterface } from "./types/common";
 import { inter } from "./ui/fonts";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Course Dashboard, built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 const RootLayout: FC<LayoutInterface> = ({ children }) => {
   return (
