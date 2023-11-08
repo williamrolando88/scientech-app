bedd7a53fdc9d870cbf057ef6470b7751be033f3
William Morales <william.rolando88@hotmail.com>
2023-11-08 07:15:16 -0500

    Install Iconify

---
+++ src/components/Shared/Iconify.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Icon } from "@iconify/react";
+import { FC } from "react";
+
+interface Props {
+  /**
+   * Icon name from https://icon-sets.iconify.design/eva/arrow-left-fill/
+   * */
+  icon: string;
+  /**
+   * Size in pixels
+   */
+  size?: string;
+  /**
+   * Support className props, compatible with TailwindCSS
+   */
+  className?: string;
+}
+
+/**
+ * Component to quick access Iconify icons database
+ */
+const Iconify: FC<Props> = ({ icon, size = 20, className = "" }) => {
+  return <Icon className={className} icon={icon} width={size} height={size} />;
+};
+
+export default Iconify;
