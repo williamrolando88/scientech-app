c5cb1771a86ebeca9388ac4c2a5e224571fb4605
William Morales <william.rolando88@hotmail.com>
2023-11-11 22:26:32 -0500

    Fix key error

--- src/components/Public/ContactPage/ContactCTA/index.tsx
+++ src/components/Public/ContactPage/ContactCTA/index.tsx
@@ -10,30 +10,22 @@ const ContactCTA: FC = () => {
       <div className="flex flex-col gap-8 border md:flex-row md:gap-0">
         <div className="flex flex-col gap-8 bg-terciary-light p-4 pb-6 text-secondary-dark md:flex-1 lg:max-w-md">
           <h2 className="text-3xl">{contactCTAContent.title}</h2>
-
           <p className="text-base">{contactCTAContent.pitch}</p>
-
           <h4 className="text-xl">{contactCTAContent.subtitle}</h4>
-
           {contactCTAContent.contactInfo.map((info) => (
-            <>
-              <div key={info.title} className="flex items-center gap-4">
-                <Iconify icon={info.icon} size="50" />
-
-                <div>
-                  <h5 className="text-lg">{info.title}</h5>
-
-                  <Link
-                    href={`mailto:${info.email}`}
-                    className="text-base underline hover:text-purple-600"
-                  >
-                    {info.email}
-                  </Link>
-
-                  <p className="text-base">{info.phoneNumber}</p>
-                </div>
+            <div key={info.title} className="flex items-center gap-4">
+              <Iconify icon={info.icon} size="50" />
+              <div>
+                <h5 className="text-lg">{info.title}</h5>
+                <Link
+                  href={`mailto:${info.email}`}
+                  className="text-base underline hover:text-purple-600"
+                >
+                  {info.email}
+                </Link>
+                <p className="text-base">{info.phoneNumber}</p>
               </div>
-            </>
+            </div>
           ))}
         </div>
 
