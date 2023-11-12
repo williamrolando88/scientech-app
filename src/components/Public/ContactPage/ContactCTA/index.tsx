5ea3c244002815991bf6c3f8201fd2e8f3aa1e1b
William Morales <william.rolando88@hotmail.com>
2023-11-11 20:42:51 -0500

    Update CTA

--- src/components/Public/ContactPage/ContactCTA/index.tsx
+++ src/components/Public/ContactPage/ContactCTA/index.tsx
@@ -8,7 +8,7 @@ const ContactCTA: FC = () => {
   return (
     <section className="relative z-20 mx-auto -mt-16 w-full max-w-7xl px-2 sm:px-6 md:mt-16 lg:px-8">
       <div className="flex flex-col gap-8 border md:flex-row md:gap-0">
-        <div className="flex flex-col gap-8 bg-terciary-light p-4 pb-6 text-black md:flex-1 lg:max-w-md">
+        <div className="flex flex-col gap-8 bg-terciary-light p-4 pb-6 text-secondary-dark md:flex-1 lg:max-w-md">
           <h2 className="text-3xl">{contactCTAContent.title}</h2>
 
           <p className="text-base">{contactCTAContent.pitch}</p>
