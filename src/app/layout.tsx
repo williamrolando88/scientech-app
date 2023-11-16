c2e58793ce2a98e1e57c43bc0a5eefe024334167
William Morales <william.rolando88@hotmail.com>
2023-11-15 20:41:02 -0500

    Add analytics (#13)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/src/lib/global.css";
 import { inter } from "@/src/lib/utils/fonts";
+import { Analytics } from "@vercel/analytics/react";
 import { Metadata } from "next";
 import { FC } from "react";
 import { LayoutInterface } from "../types/common";
@@ -14,7 +15,10 @@ export const metadata: Metadata = {
 const layout: FC<LayoutInterface> = ({ children }) => {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 };
