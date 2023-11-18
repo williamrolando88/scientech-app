07c1e8f89dc0348d5f3adac4f92961f4674f91f4
William Morales <william.rolando88@hotmail.com>
2023-11-18 10:09:04 -0500

    Fix logout button

--- src/components/Private/DashboardLayout/SideNav/LogoutButton.tsx
+++ src/components/Private/DashboardLayout/SideNav/LogoutButton.tsx
@@ -12,7 +12,7 @@ export const LogoutButton: FC<LogoutButtonProps> = ({ collapsed }) => {
     <form action={logOut} className={clsx({ "w-full": !collapsed })}>
       <button
         className={clsx(
-          "flex items-center gap-2 rounded-md bg-gray-50 px-3 py-2 text-left transition-all hover:bg-sky-100 hover:text-primary-light",
+          "flex w-full items-center gap-2 rounded-md bg-gray-50 px-3 py-2 text-left transition-all hover:bg-sky-100 hover:text-primary-light",
         )}
       >
         <Iconify icon="ic:round-logout" size="30" />
