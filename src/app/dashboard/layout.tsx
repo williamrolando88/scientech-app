9578d0f19d93c217c25a5788fc5008dace8e6896
William Morales <william.rolando88@hotmail.com>
2023-11-18 08:55:25 -0500

    Update dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,16 @@
-import DashboardLayout from "@/src/components/Private/DashboardLayout";
+import SideNav from "@/src/components/Private/DashboardLayout/SideNav";
 import { LayoutInterface } from "@/src/types/common";
 import { FC } from "react";
 
 const layout: FC<LayoutInterface> = ({ children }) => {
-  return <DashboardLayout>{children}</DashboardLayout>;
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
 };
 
 export default layout;
