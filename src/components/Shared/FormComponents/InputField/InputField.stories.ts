878a7d5f0f04833f29eb3c527afb05714480db5d
William Morales <william.rolando88@hotmail.com>
2023-11-18 08:19:55 -0500

    Add import calculator

--- src/components/Shared/FormComponents/InputField/InputField.stories.ts
+++ src/components/Shared/FormComponents/InputField/InputField.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import InputField from ".";
+import Iconify from "../../Iconify";
 
 const metaInputField: Meta<typeof InputField> = {
   title: "Primitives/InputField",
@@ -15,5 +16,12 @@ type Story = StoryObj<typeof metaInputField>;
 export const Default: Story = {
   args: {
     value: "Default input value",
+  },
+};
+
+export const WithIcons: Story = {
+  args: {
+    value: "Default input value",
+    icon: Iconify({ icon: "eva:arrow-circle-up-fill" }),
   },
 };
