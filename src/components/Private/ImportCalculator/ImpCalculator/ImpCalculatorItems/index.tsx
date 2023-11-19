cee9bec556dd253bb71cb6bbf539e68dcd6fb25b
William Morales <william.rolando88@hotmail.com>
2023-11-18 23:25:55 -0500

    Add table headers

--- src/components/Private/ImportCalculator/ImpCalculator/ImpCalculatorItems/index.tsx
+++ src/components/Private/ImportCalculator/ImpCalculator/ImpCalculatorItems/index.tsx
@@ -1,7 +1,36 @@
+import Button from "@/src/components/Shared/FormComponents/Button";
+import { Table, TableHead, TableHeaderCell, TableRow } from "@tremor/react";
 import { FC } from "react";
 
 const ImpCalculatorItems: FC = () => {
-  return <div>CalculatorItems</div>;
+  return (
+    <section className="flex items-center justify-between rounded-md border p-4">
+      <Table className="w-full">
+        <CalculatorInputsTableHeader />
+      </Table>
+    </section>
+  );
 };
 
 export default ImpCalculatorItems;
+
+const CalculatorInputsTableHeader = () => {
+  return (
+    <TableHead>
+      <TableRow>
+        <TableHeaderCell>Cant.</TableHeaderCell>
+        <TableHeaderCell className="w-100">Descripción</TableHeaderCell>
+        <TableHeaderCell>Peso U.</TableHeaderCell>
+        <TableHeaderCell>Costo U.</TableHeaderCell>
+        <TableHeaderCell>Arancel</TableHeaderCell>
+        <TableHeaderCell>Margen</TableHeaderCell>
+        <TableHeaderCell>Precio U.</TableHeaderCell>
+        <TableHeaderCell className="p-0">
+          <Button variant="success" className="h-full w-full">
+            +
+          </Button>
+        </TableHeaderCell>
+      </TableRow>
+    </TableHead>
+  );
+};
--- src/components/Private/ImportCalculator/ImpCalculator/index.tsx
+++ src/components/Private/ImportCalculator/ImpCalculator/index.tsx
@@ -1,11 +1,15 @@
 import { ImpCalculatorProvider } from "@/src/hooks/useImpCalculator";
 import { FC } from "react";
 import ImpCalculatorControllers from "./ImpCalculatorControllers";
+import ImpCalculatorItems from "./ImpCalculatorItems";
 
 const ImpCalculator: FC = () => {
   return (
     <ImpCalculatorProvider>
-      <ImpCalculatorControllers />
+      <div className="flex flex-col gap-6">
+        <ImpCalculatorControllers />
+        <ImpCalculatorItems />
+      </div>
     </ImpCalculatorProvider>
   );
 };
