e37729a4b960842f282911c8c596d57e380ab04c
William Morales <william.rolando88@hotmail.com>
2023-12-03 17:03:25 -0500

    Add reset report action

--- src/hooks/useImpCalculator.tsx
+++ src/hooks/useImpCalculator.tsx
@@ -18,10 +18,7 @@ import {
 } from "../constants/importCalculator";
 import { localStorageKey } from "../constants/keys";
 import { submitTestForm } from "../lib/actions/calculator";
-import {
-  calculateImportation,
-  getImportReport,
-} from "../lib/modules/calculator";
+import { calculateImportation, getImportReport } from "../lib/modules/calculator";
 import { ImportCalculatorValidationSchema } from "../lib/parsers/importCalculator";
 import { ImportCalculator } from "../types/calculator";
 
@@ -52,32 +49,20 @@ interface Context {
 const ImpCalculatorContext = createContext<Context>({} as Context);
 
 export const ImpCalculatorProvider = ({ children, fetchedValues }: Props) => {
-  const [calculatorInputs, setCalculatorInputs] =
-    useLocalStorage<ImportCalculator>(
-      localStorageKey.importCalculator,
-      IMPORT_CALCULATOR_INITIAL_VALUE,
-    );
+  const [calculatorInputs, setCalculatorInputs] = useLocalStorage<ImportCalculator>(
+    localStorageKey.importCalculator,
+    IMPORT_CALCULATOR_INITIAL_VALUE,
+  );
   const [totalWeight, setTotalWeight] = useState(0);
   const [totalCost, setTotalCost] = useState(0);
-  const [calculatorReport, setCalculatorReport] = useState<ApexAxisChartSeries>(
-    [],
-  );
+  const [calculatorReport, setCalculatorReport] = useState<ApexAxisChartSeries>([]);
 
-  const {
-    values,
-    errors,
-    touched,
-    handleChange,
-    resetForm,
-    setValues,
-    setFieldValue,
-  } = useFormik<ImportCalculator>({
-    initialValues: calculatorInputs || IMPORT_CALCULATOR_INITIAL_VALUE,
-    onSubmit: () => alert("submit"),
-    validationSchema: toFormikValidationSchema(
-      ImportCalculatorValidationSchema,
-    ),
-  });
+  const { values, errors, touched, handleChange, resetForm, setValues, setFieldValue } =
+    useFormik<ImportCalculator>({
+      initialValues: calculatorInputs || IMPORT_CALCULATOR_INITIAL_VALUE,
+      onSubmit: () => alert("submit"),
+      validationSchema: toFormikValidationSchema(ImportCalculatorValidationSchema),
+    });
 
   const debouncedValues = useDebounce<ImportCalculator>(values, 500);
 
@@ -121,13 +106,11 @@ export const ImpCalculatorProvider = ({ children, fetchedValues }: Props) => {
   const resetCalculator = useCallback(() => {
     setCalculatorInputs(IMPORT_CALCULATOR_INITIAL_VALUE);
     resetForm({ values: IMPORT_CALCULATOR_INITIAL_VALUE });
+    setCalculatorReport([]);
   }, [resetForm, setCalculatorInputs]);
 
   const calculate = useCallback(() => {
     const { pricesArray, articlesReport } = calculateImportation(values);
-
-    // eslint-disable-next-line no-console
-    console.log(articlesReport);
 
     setValues((prevState) => ({
       ...prevState,
@@ -143,16 +126,10 @@ export const ImpCalculatorProvider = ({ children, fetchedValues }: Props) => {
   useEffect(() => {
     setCalculatorInputs(debouncedValues);
     setTotalCost(
-      debouncedValues.items.reduce(
-        (acc, item) => acc + item.unitCost * item.quantity,
-        0,
-      ),
+      debouncedValues.items.reduce((acc, item) => acc + item.unitCost * item.quantity, 0),
     );
     setTotalWeight(
-      debouncedValues.items.reduce(
-        (acc, item) => acc + item.unitWeight * item.quantity,
-        0,
-      ),
+      debouncedValues.items.reduce((acc, item) => acc + item.unitWeight * item.quantity, 0),
     );
   }, [debouncedValues, setCalculatorInputs]);
 
