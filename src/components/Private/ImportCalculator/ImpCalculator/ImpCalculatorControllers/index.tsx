07dd819c4b67ad25b4f1227faec254a92a2153e0
William Morales <william.rolando88@hotmail.com>
2023-12-05 11:42:13 -0500

    williammorales1993/sci-10-add-a-modal-to-fill-the-description-before-saving-the

--- src/components/Private/ImportCalculator/ImpCalculator/ImpCalculatorControllers/index.tsx
+++ src/components/Private/ImportCalculator/ImpCalculator/ImpCalculatorControllers/index.tsx
@@ -3,37 +3,59 @@
 import Button from "@/src/components/Shared/FormComponents/Button";
 import Iconify from "@/src/components/Shared/Iconify";
 import { useImpCalculatorContext } from "@/src/hooks/useImpCalculator";
-import { FC } from "react";
+import { Dialog } from "@headlessui/react";
+import { FC, useState } from "react";
 
 const ImpCalculatorControllers: FC = () => {
+  const [saveModalOpen, setSaveModalOpen] = useState(false);
   const { calculate, resetCalculator } = useImpCalculatorContext();
+
+  const openSaveModal = () => {
+    setSaveModalOpen(true);
+  };
+
   return (
     <section className="flex items-center justify-between rounded-md border p-4">
       <h1 className="text-2xl">Calculadora de Importaciones</h1>
 
       <div className="flex gap-2">
-        <Button
-          type="button"
-          icon={<Iconify icon="eva:file-outline" />}
-          onClick={resetCalculator}
-        >
+        <Button type="button" icon={<Iconify icon="eva:file-outline" />} onClick={resetCalculator}>
           Nuevo
         </Button>
 
-        <Button
-          type="button"
-          icon={<Iconify icon="eva:settings-2-outline" />}
-          onClick={calculate}
-        >
+        <Button type="button" icon={<Iconify icon="eva:settings-2-outline" />} onClick={calculate}>
           Calcular
         </Button>
 
-        <Button type="submit" icon={<Iconify icon="eva:save-outline" />}>
+        <Button type="button" icon={<Iconify icon="eva:save-outline" />} onClick={openSaveModal}>
           Guardar
         </Button>
       </div>
+
+      <SaveConfirmation open={saveModalOpen} setOpen={setSaveModalOpen} />
     </section>
   );
 };
 
 export default ImpCalculatorControllers;
+
+interface SaveConfirmationProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+
+const SaveConfirmation: FC<SaveConfirmationProps> = ({ open, setOpen }) => {
+  const closeModal = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Dialog open={open} onClose={closeModal}>
+      <Dialog.Overlay />
+      <Dialog.Title>Guardar calculo</Dialog.Title>
+      <Dialog.Description>¿Desea guardar el calculo?</Dialog.Description>
+      <Button onClick={closeModal}>Cancelar</Button>
+      <Button>Guardar</Button>
+    </Dialog>
+  );
+};
