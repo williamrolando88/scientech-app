cc6d54b89aa4b8c7d86ba1704838dbcfd96175e7
William Morales <william.rolando88@hotmail.com>
2023-12-05 12:22:26 -0500

    williammorales1993/sci-10-add-a-modal-to-fill-the-description-before-saving-the (#32)
    williammorales1993/sci-10-add-a-modal-to-fill-the-description-before-saving-the
    
    Export component to different file

--- src/components/Private/ImportCalculator/ImpCalculator/ImpCalculatorControllers/index.tsx
+++ src/components/Private/ImportCalculator/ImpCalculator/ImpCalculatorControllers/index.tsx
@@ -3,35 +3,36 @@
 import Button from "@/src/components/Shared/FormComponents/Button";
 import Iconify from "@/src/components/Shared/Iconify";
 import { useImpCalculatorContext } from "@/src/hooks/useImpCalculator";
-import { FC } from "react";
+import { FC, useState } from "react";
+import { SaveConfirmation } from "./SaveConfirmation";
 
 const ImpCalculatorControllers: FC = () => {
+  const [saveModalOpen, setSaveModalOpen] = useState(false);
   const { calculate, resetCalculator } = useImpCalculatorContext();
+
+  const openSaveModal = () => {
+    setSaveModalOpen(true);
+  };
+
   return (
     <section className="flex items-center justify-between rounded-md border p-4">
       <h1 className="text-2xl">Calculadora de Importaciones</h1>
 
       <div className="flex gap-2">
-        <Button
-          type="button"
-          icon={<Iconify icon="eva:file-outline" />}
-          onClick={resetCalculator}
-        >
+        <Button type="button" icon={<Iconify icon="eva:file-outline" />} onClick={resetCalculator}>
           Nuevo
         </Button>
 
-        <Button
-          type="button"
-          icon={<Iconify icon="eva:settings-2-outline" />}
-          onClick={calculate}
-        >
+        <Button type="button" icon={<Iconify icon="eva:settings-2-outline" />} onClick={calculate}>
           Calcular
         </Button>
 
-        <Button type="submit" icon={<Iconify icon="eva:save-outline" />}>
+        <Button type="button" icon={<Iconify icon="eva:save-outline" />} onClick={openSaveModal}>
           Guardar
         </Button>
       </div>
+
+      <SaveConfirmation open={saveModalOpen} setOpen={setSaveModalOpen} />
     </section>
   );
 };
